74ad6bb75ca001ede8be8c198814be4f335d7b7a
Aaron Wright <aaron@sikosoft.com>
2024-05-27 21:56:32 +0200

    update Vite config to set 'base', update index.html to use this for asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  envPrefix: 'APP_',
-  plugins: [tsconfigPaths()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.BASE_URL || '/',
+    envPrefix: 'APP_',
+    plugins: [tsconfigPaths()],
+  };
 });
