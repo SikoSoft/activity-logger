aa153e3e5eceea682b7b80c6545786471eb1f6e4
Aaron Wright <aaron@sikosoft.com>
2025-05-13 20:00:12 +0200

    update property/entity related mock interfaces/data

--- src/mock/entity-config.ts
+++ src/mock/entity-config.ts
@@ -1,44 +1,108 @@
+export enum PropertyDataType {
+  TEXT = 'text',
+  NUMBER = 'number',
+  BOOLEAN = 'boolean',
+}
+
+export enum PropertyControlType {
+  TEXT = 'text',
+  NUMBER = 'number',
+  BOOLEAN = 'boolean',
+  SELECT = 'select',
+}
+
+export enum PropertyRenderType {
+  PLAIN_TEXT = 'plainText',
+  RICH_TEXT = 'richText',
+  IMAGE = 'image',
+  VIDEO = 'video',
+  HIDDEN = 'hidden',
+}
+
+export interface DataTypeMap {
+  [PropertyDataType.TEXT]: string;
+  [PropertyDataType.NUMBER]: number;
+  [PropertyDataType.BOOLEAN]: boolean;
+}
+
+/*
 export enum PropertyType {
   TEXT = 'text',
   NUMBER = 'number',
 }
+*/
 
+/*
 export interface PropertyDataType {
   [PropertyType.TEXT]: string;
   [PropertyType.NUMBER]: number;
 }
+*/
 
-export const propertyTypes = [
-  { id: 1, name: 'text', type: PropertyType.TEXT },
-  { id: 2, name: 'number', type: PropertyType.NUMBER },
-];
-
-export interface PropertyConfig {
+export interface CommonPropertyConfig {
   id: number;
-  type: PropertyType;
+  dataType: PropertyDataType;
+  controlType: PropertyControlType;
+  renderType: PropertyRenderType;
   name: string;
   repeat: number;
   min: number;
   max: number;
-  defaultValue: PropertyDataType[PropertyType];
+  defaultValue: DataTypeMap[PropertyDataType];
 }
 
-export interface Property {
-  propertyId: number;
-  value: PropertyDataType[PropertyType];
+export interface NumberPropertyConfig extends CommonPropertyConfig {
+  dataType: PropertyDataType.NUMBER;
+  controlType: PropertyControlType.NUMBER;
+  defaultValue: number;
 }
 
-export const properties: PropertyConfig[] = [
+export interface TextPropertyConfig extends CommonPropertyConfig {
+  dataType: PropertyDataType.TEXT;
+  controlType: PropertyControlType.TEXT;
+  defaultValue: string;
+}
+
+export interface BooleanPropertyConfig extends CommonPropertyConfig {
+  dataType: PropertyDataType.BOOLEAN;
+  controlType: PropertyControlType.BOOLEAN;
+  defaultValue: boolean;
+}
+
+export type PropertyConfig =
+  | NumberPropertyConfig
+  | TextPropertyConfig
+  | BooleanPropertyConfig;
+
+export const propertyConfigs: PropertyConfig[] = [
   {
     id: 1,
-    type: PropertyType.NUMBER,
-    name: 'reps',
+    name: 'text',
+    dataType: PropertyDataType.TEXT,
+    controlType: PropertyControlType.TEXT,
+    renderType: PropertyRenderType.PLAIN_TEXT,
     repeat: 1,
     min: 1,
     max: 1,
-    defaultValue: 1,
+    defaultValue: '',
+  },
+  {
+    id: 2,
+    name: 'reps',
+    dataType: PropertyDataType.NUMBER,
+    controlType: PropertyControlType.NUMBER,
+    renderType: PropertyRenderType.PLAIN_TEXT,
+    repeat: 1,
+    min: 1,
+    max: 1,
+    defaultValue: 0,
   },
 ];
+
+export interface Property {
+  propertyId: number;
+  value: DataTypeMap[PropertyDataType];
+}
 
 export const defaultEntityType = 1;
 
