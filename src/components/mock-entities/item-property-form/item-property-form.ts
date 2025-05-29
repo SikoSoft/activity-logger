08ccda685e9ec89a17c737b2a124467b4b241e3b
Aaron Wright <aaron@sikosoft.com>
2025-05-29 20:07:33 +0200

    cleanup in item-property-form

--- src/components/mock-entities/item-property-form/item-property-form.ts
+++ src/components/mock-entities/item-property-form/item-property-form.ts
@@ -15,6 +15,8 @@ import {
   RenderType,
 } from '@/models/Entity';
 
+import '@ss/ui/components/ss-input';
+
 const properties = propertiesJson as unknown as PropertyConfig[];
 
 @customElement('item-property-form')
@@ -45,62 +47,15 @@ export class ItemPropertyForm extends LitElement {
     itemPropertyFormProps[ItemPropertyFormProp.VALUE].default;
 
   @state()
-  get name(): string {
-    return this.getPropertyName(this.propertyId);
-  }
-
-  @state()
   get propertyConfig(): PropertyConfig {
     return properties.find(
       property => property.id === this.propertyId,
     ) as PropertyConfig;
   }
 
-  getPropertyName(id: number): string {
-    const property = properties.find(property => property.id === id);
-    return property ? property.name : '';
-  }
-
-  renderValue() {
-    const renderHandlers: Record<RenderType, () => unknown> = {
-      [RenderType.TEXT]: () => this.renderText(),
-      [RenderType.IMAGE]: () => this.renderImage(),
-    };
-
-    const renderHandler = renderHandlers[this.propertyConfig.type];
-    return renderHandler ? renderHandler() : nothing;
-  }
-
-  renderText() {
-    const value = this.value as ItemPropertyModel['value'];
-
-    return html`<span data-value=${JSON.stringify(value)} class="value">
-      ${this.propertyConfig.prefix
-        ? html`<span class="property-prefix"
-            >${this.propertyConfig.prefix}</span
-          >`
-        : nothing}<span class="property-value">${value}</span>${this
-        .propertyConfig.suffix
-        ? html`<span class="property-suffix"
-            >${this.propertyConfig.suffix}</span
-          >`
-        : nothing}
-    </span>`;
-  }
-
-  renderImage() {
-    const value = this.value as ImageProperty['value'];
-
-    return html` <span data-value=${JSON.stringify(value)} class="value">
-      <img src=${value.src} alt=${value.alt} />
-    </span>`;
-  }
-
   render() {
     return html`<div class="item-property">
-      <span data-id=${this._id} class="id">${this._id}</span>
-      <span data-name=${this.name} class="name">${this.name}</span>
-      ${this.renderValue()}
+      <ss-input placeholder=${this.propertyConfig.name}></ss-input>
     </div>`;
   }
 }
