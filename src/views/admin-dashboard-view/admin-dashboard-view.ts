b1eb6fb6cbfbd6930544a62915f08128a1a57881
Aaron Wright <aaron@sikosoft.com>
2025-11-18 21:03:17 +0100

    update view components to extend from ViewElement instead of LitElement

--- src/views/admin-dashboard-view/admin-dashboard-view.ts
+++ src/views/admin-dashboard-view/admin-dashboard-view.ts
@@ -5,9 +5,10 @@ import '@/components/admin-dashboard/admin-dashboard';
 import '@/components/login-form/login-form';
 import '@/components/logged-in/logged-in';
 import '@/components/logged-out/logged-out';
+import { ViewElement } from '@/lib/ViewElement';
 
 @customElement('admin-dashboard-view')
-export class AdminDashboardView extends LitElement {
+export class AdminDashboardView extends ViewElement {
   render(): TemplateResult {
     return html` <logged-out
         ><template><login-form></login-form></template
--- src/views/entity-form-view/entity-form-view.ts
+++ src/views/entity-form-view/entity-form-view.ts
@@ -1,13 +1,29 @@
 import { html, LitElement, TemplateResult } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, query } from 'lit/decorators.js';
 
 import '@/components/entity-form/entity-form';
 import '@/components/login-form/login-form';
 import '@/components/logged-in/logged-in';
 import '@/components/logged-out/logged-out';
+import { ViewElement } from '@/lib/ViewElement';
+import { EntityForm } from '@/components/entity-form/entity-form';
 
 @customElement('entity-form-view')
-export class EntityFormView extends LitElement {
+export class EntityFormView extends ViewElement {
+  @query('entity-form')
+  entityForm: EntityForm | undefined;
+
+  sync(reset: boolean): void {
+    console.log(
+      'EntityFormView sync called with reset=',
+      this.entityForm,
+      reset,
+    );
+    if (this.entityForm) {
+      this.entityForm.sync(reset);
+    }
+  }
+
   render(): TemplateResult {
     return html` <logged-out
         ><template><login-form></login-form></template
